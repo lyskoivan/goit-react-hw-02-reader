2f911891166f51acffc565bcbcc9033df2f769a7
lyskoivan <lysko1999i@gmail.com>
2019-12-27 00:30:12 +0200

    Updates

---
+++ precache-manifest.a08d029857de6d6e9f9514be710b5265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009cc4c2f8beceacfda96953fe7ed97b",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "87c665f77fb77ed28d52",
+    "url": "/goit-react-hw-02-reader/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c94483c5cd4518cd1c38",
+    "url": "/goit-react-hw-02-reader/static/css/main.a3bd080c.chunk.css"
+  },
+  {
+    "revision": "87c665f77fb77ed28d52",
+    "url": "/goit-react-hw-02-reader/static/js/2.7a7ca906.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-reader/static/js/2.7a7ca906.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c94483c5cd4518cd1c38",
+    "url": "/goit-react-hw-02-reader/static/js/main.b65590a1.chunk.js"
+  },
+  {
+    "revision": "7cbba3c3b2169b404c2d",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.7636bea7.js"
+  }
+]);
